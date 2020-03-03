5f70a27089a2a572318a7d8386af552bf251fbf2
Timothy Purchas <timothy.purchas@iag.com.au>
2020-03-03 14:53:09 +1100

    update libs

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Steps:
 2. Run `npm install`
 3. Open `bin/main.ts` and change the project name from `TemplateApp` to your app name.
 4. Aws Cli Login.
-5. Bootstrap, run `npx cdk deploy App-${ProjectName}-${AccountName}-CiCd` (Please change to your project name and account name).
+5. Bootstrap, run `npx cdk deploy Cosmos-App-${ProjectName}-Galaxy-${AccountName}-SolarSystem-CiCd` (Please change to your project name and account name).
 6. Update git remote url `git remote set-url origin "https://git-codecommit.ap-southeast-2.amazonaws.com/v1/repos/app-${ProjectName}-cdk-repo"` (Please change Project name and region etc as required).
 7. Commit your changes and Push your custom version of the template to your app cdk repo, `git commit && git push`
 8. Done: Now use the app cdk pipeline to deploy any further changes to your app cdk code (Pipeline Named `App-${ProjectName}-Cdk-Pipeline`)
--- bin/main.ts
+++ bin/main.ts
@@ -14,24 +14,19 @@ const app = new App();
 const mgtEnvConfig = { account: "1111", region: "ap-southeast-2" };
 const devEnvConfig = { account: "2222", region: "ap-southeast-2" };
 
-// Project Level Infra
 const cosmos = new AppCosmosStack(app, "DemoApp", {
   env: mgtEnvConfig
 });
 
-// Account Level Infra
 const mgtGalaxy = new AppGalaxyStack(cosmos, "Mgt");
 
-// CiCd Level Infra
 const ciCd = new AppCiCdSolarSystemStack(mgtGalaxy);
 
-// Dev Account Level Infra
 const devGalaxy = new AppGalaxyStack(cosmos, "Dev", {
   env: devEnvConfig
 });
 cosmos.EcrRepo.grantPull(new AccountPrincipal(devGalaxy.account));
 
-// Dev App Env Level Infra
 const dev = new AppSolarSystemStack(devGalaxy, "Dev", {
   tag: process.env.APP_BUILD_VERSION || "latest"
 });
@@ -40,6 +35,5 @@ ciCd.addCdkDeployEnvStageToPipeline({
   isManualApprovalRequired: false
 });
 
-// Tst App Env Level Infra
 const tst = new AppSolarSystemStack(devGalaxy, "Tst");
 ciCd.addCdkDeployEnvStageToPipeline({ solarSystem: tst });
